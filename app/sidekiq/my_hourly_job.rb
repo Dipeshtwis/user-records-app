67dd8b3848eba4d0c92921dc09d6b6858e6e6af1
Dipeshtwis <deepesh.97kumar@gmail.com>
2024-01-15 03:21:12 +0530

    handle error for API issue

--- app/sidekiq/my_hourly_job.rb
+++ app/sidekiq/my_hourly_job.rb
@@ -2,20 +2,31 @@ class MyHourlyJob
   include Sidekiq::Job
 
   def perform(*args)
-    user_records = fetch_random_user_records
+    begin
+      user_records = fetch_random_user_records
 
-    user_records.each do |user_record|
-      update_or_create_user(user_record)
+      user_records.each do |user_record|
+        update_or_create_user(user_record)
+      end
+
+      store_counts_in_redis(user_records)
+    rescue StandardError => e
+      puts "Error in perform: #{e.message}"
     end
-
-    store_counts_in_redis(user_records)
   end
 
   private
 
   def fetch_random_user_records
     response = RestClient.get('https://randomuser.me/api/?results=20')
-    JSON.parse(response.body)['results']
+    if response.code == 200
+      records = JSON.parse(response.body)['results']
+      return records
+    else
+      raise StandardError, "API request failed with status: #{response.code}"
+    end
+  rescue RestClient::ExceptionWithResponse => e
+    raise StandardError, "RestClient error: #{e.response}"
   end
 
   def update_or_create_user(user_record)
