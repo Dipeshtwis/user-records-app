669eab7d29db8a60052a5cb101aa4632434675cf
Dipeshtwis <deepesh.97kumar@gmail.com>
2024-01-16 01:22:12 +0530

    Fix daily record gender count

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     gender = @user.gender
 
-    # Update the corresponding count in DailyRecord based on the user's gender
-    DailyRecord.increment_counter("#{gender}_count", Date.today)
+    # Find or initialize the DailyRecord for the current date
+    daily_record = DailyRecord.find_or_initialize_by(date: Date.today)
+
+    # Decrement the corresponding count in DailyRecord based on the user's gender
+    daily_record.update("#{gender}_count": daily_record["#{gender}_count"].to_i - 1) if daily_record.persisted?
+    daily_record.save
 
     @user.destroy
 
